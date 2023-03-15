8e3e63286bfb3a0ccc71b8d928c6047724fa549e
iLoveDataJjia <ilovedata.jjia@gmail.com>
2023-03-15 07:41:01 +0100

    [DEV] ✨ WIP on user hooks

--- frontend/src/context/user/UserHk.tsx
+++ frontend/src/context/user/UserHk.tsx
@@ -1,9 +1,47 @@
 import { useContext } from "react";
 import { UserContext, IUserContext } from "./UserCtx";
+import { usePost } from "../../hooks";
+import { TokenDtoOut } from "../../data";
+import { useEffect } from "react";
 
 /**
- * Use user hook.
+ * User context hook.
  */
-export function useUser() {
+export function useUserContext() {
   return useContext(UserContext) as IUserContext;
 }
+
+/**
+ * User connexion hook.
+ */
+export function useUserConnect() {
+  // Pre-requisites
+  const { accessToken, expireAt, refreshToken, setAccessToken, setExpireAt, setRefreshToken } = useUserContext();
+  const { post, isLoading, data } = usePost<TokenDtoOut>("/user/login", "TokenDtoOut");
+
+  // Effect running on log in changes
+  useEffect(() => {
+    switch (data?.kind) {
+      case "TokenDtoOut":
+        setAccessToken(data.accessToken);
+        setExpireAt(data.expireAt);
+        setRefreshToken(data.refreshToken);
+        break;
+      case "AppException":
+      default:
+        break;
+    }
+  }, [data]);
+
+  // Return
+  return {
+    connect: (email: string, pwd: string) => post({ email: email, pwd: pwd }),
+    isConnecting: isLoading,
+    isConnected: accessToken !== undefined && expireAt !== undefined && refreshToken !== undefined,
+  };
+}
+
+/**
+ * User info hook.
+ */
+export function useUser() {}
