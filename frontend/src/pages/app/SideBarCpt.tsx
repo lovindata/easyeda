9a3fe34286664ff3cc8319536407ca479162d755
iLoveDataJjia <ilovedata.jjia@gmail.com>
2023-04-01 18:12:38 +0200

    [DEV] ✨ WIP on profil icon

--- frontend/src/pages/app/SideBarCpt.tsx
+++ frontend/src/pages/app/SideBarCpt.tsx
@@ -13,7 +13,7 @@ export default function SideBarCpt() {
   // Render
   return (
     <div className="flex h-screen w-12 flex-col">
-      <div className="flex grow flex-col">
+      <div className="flex grow flex-col justify-center">
         <IconTabLink to="connections" title="Connections" svg={Conn} isCurrent={currentPath == "/app/connections"} />
         <IconTabLink to="pipelines" title="Pipelines" svg={Pipeline} isCurrent={currentPath == "/app/pipelines"} />
       </div>
@@ -64,8 +64,35 @@ function IconUser() {
 
   // Render
   return (
-    <div className="flex flex-col justify-center p-2.5">
-      <Profil />
-    </div>
+    <div className="relative flex flex-col justify-end p-2.5">
+      <Profil className="peer flex fill-primary hover:brightness-150" />
+      <span
+        className={
+          "absolute bottom-0 right-0 h-2.5 w-2.5 rounded-full border-2 border-base-100" +
+          (isRetrieving ? " bg-warning" : " bg-success")
+        }
+      />
+      <div
+        className="pointer-events-none absolute left-full ml-1 flex origin-bottom-left scale-0 select-none
+        space-x-1.5 rounded bg-neutral px-1.5 py-1 align-middle text-xs
+        font-thin shadow transition-all duration-300 ease-in-out peer-hover:scale-100"
+      >
+        <div className="flex flex-col">
+          <h1 className="text-sm font-semibold">{user?.username}</h1>
+          <p className="italic">#{user?.id}</p>
+        </div>
+        <div className="flex flex-col">
+          <div className="flex space-x-1">
+            <Conn className="w-5 fill-primary" />
+            <p className="text-error">?/?</p>
+          </div>
+          <div className="flex space-x-1">
+            <Pipeline className="w-5 fill-primary" />
+            <p>?/?</p>
+          </div>
+        </div>
+      </div>
+      </div>
+
   );
 }
