5c09fb1a174ff5d22d8887363e5afb9baa90e7a3
iLoveDataJjia <ilovedata.jjia@gmail.com>
2023-04-10 12:14:54 +0200

    [DEV] ✨ Remove old frontend

--- frontend/_old/frontend/src/pages/Profil.tsx
+++
@@ -1,8 +0,0 @@
-import { motion } from "framer-motion";
-
-// Profil
-export const Profil = () => (
-  <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="m-auto">
-    Profil in UX design...
-  </motion.div>
-);
--- frontend/_old/frontend/src/pages/Ranking.tsx
+++
@@ -1,8 +0,0 @@
-import { motion } from "framer-motion";
-
-// Ranks
-export const Ranking = () => (
-  <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} className="m-auto">
-    Ranks to build UI...
-  </motion.div>
-);
